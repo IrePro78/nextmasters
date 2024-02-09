844755f88f73668d194de046d9e340c8513eb1af
IrePro78 <gos3r78@gmail.com>
2024-02-09 14:28:24 +0100

    Refactor layout and product list components

--- src/ui/molecules/ProductListItem.tsx
+++ src/ui/molecules/ProductListItem.tsx
@@ -4,11 +4,11 @@ import { type ProductItemType } from '@/ui/types/types';
 
 export const ProductListItem = ({ product }: ProductItemType) => {
 	return (
-		<li>
+		<div>
 			<article>
 				<ProductCoverCoverImage {...product.coverImage} />
 				<ProductListItemDescription product={product} />
 			</article>
-		</li>
+		</div>
 	);
 };
--- src/ui/organism/ProductList.tsx
+++ src/ui/organism/ProductList.tsx
@@ -7,10 +7,13 @@ export const ProductList = ({
 	products: ProductItemType[];
 }) => {
 	return (
-		<ul className="gap--8 grid grid-cols-1 gap-8 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+		<div
+			className=" gap--8 grid grid-cols-1 gap-8 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4"
+			data-testid="products-list"
+		>
 			{products.map((product) => (
 				<ProductListItem key={product.id} {...product} />
 			))}
-		</ul>
+		</div>
 	);
 };
