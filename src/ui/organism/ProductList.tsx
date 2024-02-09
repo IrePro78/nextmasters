c9df8586b3c2f2dc7984ed315cace33098fae28b
IrePro78 <78473540+IrePro78@users.noreply.github.com>
2024-02-09 14:28:13 +0100

    Task no.1 (#2)
    * Fix formatting issues in code
    
    * Delete unused images and refactor product list components
    
    * Refactor layout and product list components

--- src/ui/organism/ProductList.tsx
+++ src/ui/organism/ProductList.tsx
@@ -7,10 +7,21 @@ export const ProductList = ({
 	products: ProductItemType[];
 }) => {
 	return (
+
+		<div
+			className=" gap--8 grid grid-cols-1 gap-8 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4"
+			data-testid="products-list"
+		>
+			{products.map((product) => (
+				<ProductListItem key={product.id} {...product} />
+			))}
+		</div>
+
 		<ul className="gap--8 grid grid-cols-1 gap-8 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
 			{products.map((product) => (
 				<ProductListItem key={product.id} {...product} />
 			))}
 		</ul>
+
 	);
 };
