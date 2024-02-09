9b32cb8a69d2476eaabe1cae632425858f87605c
IrePro78 <gos3r78@gmail.com>
2024-02-09 14:33:03 +0100

    Remove unnecessary list item tags in ProductListItem and ProductList components

--- src/ui/molecules/ProductListItem.tsx
+++ src/ui/molecules/ProductListItem.tsx
@@ -5,13 +5,10 @@ import { type ProductItemType } from '@/ui/types/types';
 export const ProductListItem = ({ product }: ProductItemType) => {
 	return (
 		<div>
-		<li>
 			<article>
 				<ProductCoverCoverImage {...product.coverImage} />
 				<ProductListItemDescription product={product} />
 			</article>
-
 		</div>
-		</li>
 	);
 };
--- src/ui/organism/ProductList.tsx
+++ src/ui/organism/ProductList.tsx
@@ -7,7 +7,6 @@ export const ProductList = ({
 	products: ProductItemType[];
 }) => {
 	return (
-
 		<div
 			className=" gap--8 grid grid-cols-1 gap-8 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4"
 			data-testid="products-list"
@@ -16,12 +15,5 @@ export const ProductList = ({
 				<ProductListItem key={product.id} {...product} />
 			))}
 		</div>
-
-		<ul className="gap--8 grid grid-cols-1 gap-8 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-			{products.map((product) => (
-				<ProductListItem key={product.id} {...product} />
-			))}
-		</ul>
-
 	);
 };
