5a1d624687c230a878a1fe76530d269456a8f2c7
IrePro78 <78473540+IrePro78@users.noreply.github.com>
2024-02-18 20:40:13 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -74,7 +74,6 @@ const products = [
 		},
 	},
 ];
-=======
 import React from 'react';
 
 export default function HomePage() {
@@ -83,12 +82,11 @@ export default function HomePage() {
 		<section className="mx-auto max-w-md bg-stone-400 p-12 sm:max-w-2xl sm:py-16 md:max-w-4xl lg:max-w-7xl">
 			<ProductList products={products} />
 		</section>
-<>
+
 			<div className=" mx-auto min-h-full bg-zinc-400 p-12 text-zinc-900 sm:max-w-2xl sm:py-16 md:max-w-4xl lg:max-w-7xl">
 				<h1>Welcome to the Home Page</h1>
 				<p>This is the content of the Home Page.</p>
 			</div>
-		</>
- main
+	
 	);
 }
