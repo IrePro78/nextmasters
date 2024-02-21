ebb29424686320ef1b7ce708a9bff3a0886537d6
IrePro78 <78473540+IrePro78@users.noreply.github.com>
2024-02-21 09:18:15 +0100

    Task no.2 (#5)
    * Update code formatting and fix minor bugs
    
    * Update product name display
    
    * Add description field to ProductItemType and update dependencies
    
    * Update imports and URLs
    
    * Refactor API URLs and implement pagination
    
    * Refactor pagination logic and update product list rendering
    
    * Remove console.log statements and commented code
    
    * Refactor generateStaticParams function to include all products
    
    * Remove unused code for generating static params
    
    * Refactor home page layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -79,11 +79,16 @@ import React from 'react';
 
 export default function HomePage() {
 	return (
+
+		<>
+			<div className=" mx-auto min-h-screen bg-zinc-400 p-12 text-center text-zinc-900">
+
 		<section className="mx-auto max-w-md bg-stone-400 p-12 sm:max-w-2xl sm:py-16 md:max-w-4xl lg:max-w-7xl">
 			<ProductList products={products} />
 		</section>
 
 			<div className=" mx-auto min-h-full bg-zinc-400 p-12 text-zinc-900 sm:max-w-2xl sm:py-16 md:max-w-4xl lg:max-w-7xl">
+
 				<h1>Welcome to the Home Page</h1>
 				<p>This is the content of the Home Page.</p>
 			</div>
