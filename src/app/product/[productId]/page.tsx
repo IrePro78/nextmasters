9bc7a97f345ea306427880b9edb3b26585143d5c
IrePro78 <gos3r78@gmail.com>
2024-02-21 08:41:50 +0100

    Remove unused code for generating static params

--- src/app/product/[productId]/page.tsx
+++ src/app/product/[productId]/page.tsx
@@ -1,16 +1,16 @@
 import { Suspense } from 'react';
 import { type Metadata } from 'next';
-import { getProductById, getProductsList } from '@/api/products';
+import { getProductById } from '@/api/products';
 import { ProductCoverCoverImage } from '@/components/atoms/ProductCoverCoverImage';
 import { ProductListItemDescription } from '@/components/atoms/ProductListItemDescription';
 import { SuggestedProducts } from '@/components/organism/SuggestedProducts';
 
-export const generateStaticParams = async () => {
-	const products = await getProductsList();
-	return products.map((product) => ({
-		productId: product.id,
-	}));
-};
+// export const generateStaticParams = async () => {
+// 	const products = await getProductsList();
+// 	return products.map((product) => ({
+// 		productId: product.id,
+// 	}));
+// };
 
 export const generateMetadata = async ({
 	params,
