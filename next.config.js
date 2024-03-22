c48572ad6238e153ebed6e32c7b7359725d64ac1
Irek <gos3r78@gmail.com>
2024-03-22 20:56:47 +0100

    Add localhost as a static asset source

--- next.config.js
+++ next.config.js
@@ -15,6 +15,11 @@ const nextConfig = {
 				hostname: 'static-ourstore.hyperfunctor.com',
 				pathname: '**',
 			},
+			{
+				protocol: 'http',
+				hostname: 'localhost',
+				pathname: '**',
+			},
 		],
 	},
 	redirects: async () => {
